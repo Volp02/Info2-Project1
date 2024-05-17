917eb999d84a39eb4c7e852af0654e231b62cead
volp02 <bila1015@h-ka.de>
2024-05-17 23:56:48 +0200

    modified:   main.cpp
    added Bubble sort to sort by Pclass and
    added sort by gender

--- main.cpp
+++ main.cpp
@@ -80,7 +80,22 @@ int main()
     cout << "GiniImpurity:" << calcBinaryGini(calcSurvProp(data)) << endl;
 
 
-    sortVectorAttribute(data, 0);
+    sortVectorAttribute(data, 1);
+
+    for (int i = 0; data[i].Num > 0; i++) {
+        cout << "age of: " << i << ": " << data[i].Age << endl;
+        cout << "Num of: " << i << ": " << data[i].Num << endl;
+        cout << "Class of: " << i << ": " << data[i].Pclass << endl;
+        cout << "Survived of: " << i << ": " << data[i].Surv << endl;
+        cout << "Sex of: " << i << ": " << data[i].Sex << endl;
+        cout << "Sibsp of: " << i << ": " << data[i].Sibl << endl;
+        cout << "Parch of: " << i << ": " << data[i].Paren << endl;
+        cout << "Fare of: " << i << ": " << data[i].Fare << endl;
+        cout << endl;
+
+    }
+
+        sortVectorAttribute(data, 2);
 
     for (int i = 0; data[i].Num > 0; i++) {
         cout << "age of: " << i << ": " << data[i].Age << endl;

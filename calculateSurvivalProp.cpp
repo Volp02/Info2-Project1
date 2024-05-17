7d247b359b1acc17905e6c628671f694cf497cbb
volp02 <bila1015@h-ka.de>
2024-05-17 18:11:11 +0200

    modified:   calculateSurvivalProp.cpp     fixed retrun type error

--- calculateSurvivalProp.cpp
+++ calculateSurvivalProp.cpp
@@ -1,13 +1,15 @@
 #include <vector>
 #include "calculateSurvivalProp.h"
 
+//#include <iostream>
+//using namespace std;
 
 float calcSurvProp(const std::vector<Passenger>& dataSet) {
     int survived = 0;
 
     for (int i = 0; i < dataSet.size(); ++i) {
-        survived += dataSet[i].Surv;
-    // Process the passenger data here
+        if (dataSet[i].Surv) survived++;
+        //cout << "debug: SurvCount:" << survived << " and added: " << dataSet[i].Surv << endl;
     } 
-    return survived / dataSet.size();
+    return float(survived) / dataSet.size();
 }

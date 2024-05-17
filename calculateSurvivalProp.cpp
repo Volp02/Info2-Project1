6e7ccb22a627c028414f46d3d991b98a4cd95844
volp02 <bila1015@h-ka.de>
2024-05-17 18:12:57 +0200

    fix

--- calculateSurvivalProp.cpp
+++ calculateSurvivalProp.cpp
@@ -1,13 +1,15 @@
 #include <vector>
 #include "calculateSurvivalProp.h"
 
+//#include <iostream>
+//using namespace std;
 
 float calcSurvProp(const std::vector<Passenger>& dataSet) {
     int survived = 0;
 
     for (int i = 0; i < dataSet.size(); ++i) {
-        survived += dataSet[i].Surv;
-    // Process the passenger data here
-    } 
-    return survived / dataSet.size();
+        if (dataSet[i].Surv) survived++;
+        //cout << "debug: SurvCount:" << survived << " and added: " << dataSet[i].Surv << endl;
+    }
+    return float(survived) / dataSet.size();
 }

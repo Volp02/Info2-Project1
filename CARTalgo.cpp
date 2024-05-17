37f50c9e108e0d8c5743fe4222b9409fe6bf5ab9
volp02 <bila1015@h-ka.de>
2024-05-17 19:46:05 +0200

    create CARTalgo files
    renamed:    CARTalgo.cpp -> CARTalgo.cpp-tmp
    	renamed:    CARTalgo.h -> CARTalgo.h-tmp

--- CARTalgo.cpp
+++ CARTalgo.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-vector<Passenger> sortVectorAttribute(const vector<Passenger>& data, int attribute){
+vector<Passenger> sortVectorAttribute(const vector<Passenger>& data, int attribute) {
 
     vector<Passenger> sortedData;
     sortedData.assign(data.begin(), data.end());        //creates new vector and assigns data to it
@@ -14,38 +14,38 @@ vector<Passenger> sortVectorAttribute(const vector<Passenger>& data, int attribu
     //1 = Class, 2 = Sex, 3 = Age, 4 = Sibl, 5 = Parent, 6 = Fare
 
 
-    switch (attribute){
-        case (1):{
-            //sort by Class
+    switch (attribute) {
+    case (1): {
+        //sort by Class
 
-        }
-        case (2):{
-            //sort by Sex
+    }
+    case (2): {
+        //sort by Sex
 
-        }
-        case (3):{
-            //sort by Age
-            
-            break;
-        }
-        case (4):{
-            //sort by Sibl
-            
-            break;
-        }
-        case (5):{
-            //sort by Parent
-            
-            break;
-        }
-        case (6):{
-            //sort by Fare
-            
-            break;
-        }
-        default:
-            std::cout << "Error: Invalid attribute for sorting" << endl;
-            break;
+    }
+    case (3): {
+        //sort by Age
+
+        break;
+    }
+    case (4): {
+        //sort by Sibl
+
+        break;
+    }
+    case (5): {
+        //sort by Parent
+
+        break;
+    }
+    case (6): {
+        //sort by Fare
+
+        break;
+    }
+    default:
+        std::cout << "Error: Invalid attribute for sorting" << endl;
+        break;
     }
 
     return sortedData;

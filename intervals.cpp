ea6e0b6974e199967976ff91ed4857f49348fcc5
rungeAm <rungeandreasm@gmail.com>
2024-05-22 17:24:35 +0200

    erster try split

---
+++ intervals.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include "CARTalgo.h"
+#include "intervals.h"
+#include "readFile.h" // Include the header file for the Passenger struct
+
+using namespace std;
+
+
+
+vector<std::vector<Passenger>> splitVector(const std::vector<Passenger> &data, int attribute)
+{
+    std::vector<Passenger> sortedData;
+    std::vector<Passenger> linkeSeite;
+    std::vector<Passenger> rechteSeite;
+
+    std::vector<Passenger> *linkeSeitePtr = &linkeSeite;
+    std::vector<Passenger> *rechhteSeitePtr = &rechteSeite;
+
+    std::vector < std::vector < Passenger>> splitInfo{linkeSeitePtr, rechhteSeitePtr };
+
+    sortedData.assign(data.begin(), data.end()); //creates new vector and assigns data to it
+
+
+    return splitInfo;
+
+    //vector für linke seite
+    //vector für rechte seite
+
+}

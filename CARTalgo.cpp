17ba9192bca106d6e3948e727bf0ad7d08761d41
Volp02 <42917599+Volp02@users.noreply.github.com>
2024-05-22 19:54:10 +0200

    SortingAlgorithmus (#6)
    Co-authored-by: MatthiasF7502 <frma1059@h-ka.de>

--- CARTalgo.cpp
+++ CARTalgo.cpp
@@ -19,73 +19,90 @@ std::vector<Passenger> swapNext(const std::vector<Passenger>& data, int i) {
 }
 
 
-std::vector<Passenger> sortVectorAttribute(const std::vector<Passenger>& data, int attribute) {
+std::vector<Passenger> sortVectorAttribute(const std::vector<Passenger> &data, int attribute)
+{
 
     std::vector<Passenger> sortedData;
     std::vector<Passenger> helperVector;
     std::vector<Passenger> helperVector2;
-    sortedData.assign(data.begin(), data.end());        //creates new vector and assigns data to it
+    sortedData.assign(data.begin(), data.end()); //creates new vector and assigns data to it
 
     //1 = Class, 2 = Sex, 3 = Age, 4 = Sibl, 5 = Parent, 6 = Fare
 
 
-    switch (attribute) {
-    case (1): {
-        for (int pass = 0; pass < sortedData.size() - 1; ++pass) {
-            bool swapped = false; // Flag to check if any swaps occurred in this pass
-
-            for (int i = 0; i < sortedData.size() - 1 - pass; ++i) { // Use i for inner loop
-                if (sortedData[i].Pclass < sortedData[i + 1].Pclass) {
-                    swapNext(sortedData, i);
-                    swapped = true;
+    switch (attribute)
+    {
+    case (1):
+    {
+        //sort by Pclass
+        {
+            int temp;
+            do
+            {
+                temp = sortedData[0].Pclass;
+                for (std::size_t j = 1; j < sortedData.size(); j++)
+                {
+                    if (sortedData[j].Pclass < sortedData[j - 1].Pclass)
+                    { // vertauschen
+                        temp = sortedData[j - 1].Pclass;
+                        sortedData[j - 1].Pclass = sortedData[j].Pclass;
+                        sortedData[j].Pclass = temp;
+                    }
                 }
-            }
-
-            if (!swapped) break; // If no swaps in this pass, the array is already sorted
+            } while (temp != sortedData[0].Pclass); // false = keine Vertauschung
         }
 
 
-    }
-    case (2): {
+    case (2):
+    {
         //sort by Sex
-        for (int i = sortedData.size() - 1; i >= 0; i--) {
+        for (int i = sortedData.size() - 1; i >= 0; i--)
+        {
 
-            if (sortedData[i].Sex) {
+            if (sortedData[i].Sex)
+            {
                 Passenger tmp = sortedData[i];
                 helperVector.push_back(tmp);
             }
-            if(!sortedData[i].Sex) {
+            if (!sortedData[i].Sex)
+            {
                 Passenger tmp = sortedData[i];
                 helperVector2.push_back(tmp);
             };
         }
         sortedData.clear();
 
-        for (int i = 0; i < helperVector.size(); i++){
+        for (int i = 0; i < helperVector.size(); i++)
+        {
             sortedData.push_back(helperVector[i]);
         }
 
-        for (int i = 0; i < helperVector2.size(); i++) {
+        for (int i = 0; i < helperVector2.size(); i++)
+        {
             sortedData.push_back(helperVector2[i]);
         }
     }
-                
-    case (3): {
+
+    case (3):
+    {
         //sort by Age
 
         break;
     }
-    case (4): {
+    case (4):
+    {
         //sort by Sibl
 
         break;
     }
-    case (5): {
+    case (5):
+    {
         //sort by Parent
 
         break;
     }
-    case (6): {
+    case (6):
+    {
         //sort by Fare
 
         break;
@@ -95,5 +112,7 @@ std::vector<Passenger> sortVectorAttribute(const std::vector<Passenger>& data, i
         break;
     }
 
-    return sortedData;
-};
+        return sortedData;
+    };
+}
+
